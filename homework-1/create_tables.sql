5196bd11aa699f8b6ab728c6dfcff78c2d3945a4
WHAT <ygyshka0@gmail.com>
2023-08-05 20:21:18 +0700

    cod to create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+
+create table customers
+(
+	customer_id varchar(100) primary key,
+	company_name varchar(100) not null,
+	contact_name varchar(100)
+);
+
+create table employers
+(
+	employer_id int primary key,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	title varchar(100) not null,
+	birth_date varchar(50),
+	notes text
+);
+
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(100) references customers(customer_id),
+	employer_id int not null references employers(employer_id),
+	order_date varchar(50),
+	ship_city varchar(100) not null
+);
+
