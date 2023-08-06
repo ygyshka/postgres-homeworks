1bee282661e77c38f7cb7699054e96e73474a11c
WHAT <ygyshka0@gmail.com>
2023-08-06 12:41:11 +0700

    homework-2

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,14 +1,20 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT ...
+SELECT * from orders
+where ship_country in ('France', 'Germany', 'Spain')
 
 -- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-
+select distinct ship_country, ship_city from orders
+order by  ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
+select avg(shipped_date - order_date) from orders
+WHERE ship_country = 'Germany'
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
-
+select min(unit_price), max(unit_price) from products
+where discontinued = 0
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+select min(unit_price), max(unit_price) from products
+where discontinued = 0 and units_in_stock >= 20
--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -1,14 +1,16 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. "имя контакта" и "город" (contact_name, country) из таблицы customers (только эти две колонки)
-SELECT ...
+SELECT contact_name, country from customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
 
-
+SELECT order_id, (shipped_date - order_date) from orders
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
-
+select distinct city from customers
+where city is not null
 
 -- 4. количество заказов (таблица orders)
-
+select count(*) from orders
 
 -- 5. количество стран, в которые отгружался товар (таблица orders, колонка ship_country)
+select count(distinct ship_country) from orders
